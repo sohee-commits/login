16e62cfa3a1c910fcc1b50f984caa1cb02056f0e
소희 <105264974+sohee-commits@users.noreply.github.com>
2023-12-20 20:26:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 function loginCheck() {
-    var login = document.forms["login"]["lg"].value;
-    var password = document.forms["login"]["pw"].value;
+  var login = document.forms[`login`][`lg`].value;
+  var password = document.forms[`login`][`pw`].value;
 
-    if (login != 'admin' && password != 'meow') {
-        alert('неверные логин и пароль');
-        return false;
-      }
-
-    if (login == 'admin' && password === 'meow') {
-      alert('вход выполнен успешно');
-      return false;
-    }
-
-    else if (login != 'admin') {
-        alert('неверный логин');
-        return false;
-    }
-
-    else if (password != 'meow') {
-        alert('неверный пароль');
-        return false;
-    }
-
-    else {
-        alert('ошибка');
-        return false;
-    }
+  if (login == `admin` && password === `meow`) {
+    alert(`succesfull login`);
+    return false;
+  } else if (login != `admin` && password != `meow`) {
+    alert(`incorrect login and password`);
+    return false;
+  } else if (login != `admin`) {
+    alert(`incorrect login`);
+    return false;
+  } else if (password != `meow`) {
+    alert(`incorrect password`);
+    return false;
+  } else {
+    alert(`ошибка`);
+    return false;
   }
+}
